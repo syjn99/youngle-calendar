491f264c822823ec809df20956ba5679ffab0984
syjn99 <syjn99@gmail.com>
2022-08-26 18:37:03 +0900

    Navbar center align

--- components/DayNavbar.js
+++ components/DayNavbar.js
@@ -40,21 +40,27 @@ export const DayNavbar = ({ date }) => {
   return (
     <>
       <nav className="h-16 bg-indigo-300 flex justify-between items-center font-bold text-white">
-        <h1 className='bg-indigo-400  m-3 p-2 border hover:cursor-pointer shadow hover:bg-indigo-500' onClick={() => router.push('/')}>Youngle Calendar</h1>
-        <button className=" bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => router.push(todayRoute)}>오늘</button>
-        <button className="text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => router.push(yesterdayRoute)}> {<FontAwesomeIcon icon={faAngleLeft} />}</button>
-        <h2 className='text-xl'>{date.year}년 {date.month}월 {date.day}일</h2>
-        <button className="text-xl mr-1/7 bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => router.push(tomorrowRoute)}>{<FontAwesomeIcon icon={faAngleRight} />}</button>
-        <select
-          defaultValue={'일'}
-          className='text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full'
-          onChange={onViewChangeClicked}
-        >
-          <option value='일'>일</option>
-          <option value='주'>주</option>
-          <option value='월'>월</option>
-        </select>
-        <button className=" bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full mr-10" onClick={() => setModalOpen(!modalOpen)}>{<FontAwesomeIcon icon={faPlus} />}</button>
+        <div className='flex w-1/3 justify-around items-center'>
+          <h1 className='bg-indigo-400  p-2 border hover:cursor-pointer shadow hover:bg-indigo-500' onClick={() => router.push('/')}>Youngle Calendar</h1>
+          <button className=" bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => router.push(todayRoute)}>오늘</button>
+        </div>
+        <div className='flex w-1/3 justify-around items-center'>
+          <button className="text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => router.push(yesterdayRoute)}> {<FontAwesomeIcon icon={faAngleLeft} />}</button>
+          <h2 className='text-xl'>{date.year}년 {date.month}월 {date.day}일</h2>
+          <button className="text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => router.push(tomorrowRoute)}>{<FontAwesomeIcon icon={faAngleRight} />}</button>
+        </div>
+        <div className='flex w-1/3 justify-around items-center'>
+          <select
+            defaultValue={'일'}
+            className='text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full'
+            onChange={onViewChangeClicked}
+          >
+            <option value='일'>일</option>
+            <option value='주'>주</option>
+            <option value='월'>월</option>
+          </select>
+          <button className=" bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => setModalOpen(!modalOpen)}>{<FontAwesomeIcon icon={faPlus} />}</button>
+        </div>
       </nav>
       {modalOpen && (
         <Modal closeModal={closeModal}>
--- components/Navbar.js
+++ components/Navbar.js
@@ -30,21 +30,28 @@ export const Navbar = () => {
   return (
     <>
       <nav className="h-16 bg-indigo-300 flex justify-between items-center font-bold text-white">
-        <h1 className='bg-indigo-400  m-3 p-2 border hover:cursor-pointer shadow hover:bg-indigo-500' onClick={() => router.push('/')}>Youngle Calendar</h1>
-        <button className=" bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => dispatch(todayMonth())}>오늘</button>
-        <button className="text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => dispatch(prevMonth())}> {<FontAwesomeIcon icon={faAngleLeft} />}</button>
-        <h2 className='text-xl'>{year}년 {month + 1}월</h2>
-        <button className="text-xl mr-1/7 bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => dispatch(nextMonth())}>{<FontAwesomeIcon icon={faAngleRight} />}</button>
-        <select
-          defaultValue={'월'}
-          className='text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full'
-          onChange={onViewChangeClicked}
-        >
-          <option value='일'>일</option>
-          <option value='주'>주</option>
-          <option value='월'>월</option>
-        </select>
-        <button className=" bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full mr-10" onClick={() => setModalOpen(!modalOpen)}>{<FontAwesomeIcon icon={faPlus} />}</button>
+        <div className='flex w-1/3 justify-around items-center'>
+          <h1 className='bg-indigo-400  p-2 border hover:cursor-pointer shadow hover:bg-indigo-500' onClick={() => router.push('/')}>Youngle Calendar</h1>
+          <button className=" bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => dispatch(todayMonth())}>오늘</button>
+        </div>
+        <div className='flex w-1/3 justify-around items-center'>
+          <button className="text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => dispatch(prevMonth())}> {<FontAwesomeIcon icon={faAngleLeft} />}</button>
+          <h2 className='text-xl'>{year}년 {month + 1}월</h2>
+          <button className="text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full" onClick={() => dispatch(nextMonth())}>{<FontAwesomeIcon icon={faAngleRight} />}</button>
+        </div>
+
+        <div className='flex w-1/3 justify-around items-center'>
+          <select
+            defaultValue={'월'}
+            className='text-xl bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full'
+            onChange={onViewChangeClicked}
+          >
+            <option value='일'>일</option>
+            <option value='주'>주</option>
+            <option value='월'>월</option>
+          </select>
+          <button className=" bg-indigo-500 hover:bg-indigo-900 py-1 px-2 rounded-full " onClick={() => setModalOpen(!modalOpen)}>{<FontAwesomeIcon icon={faPlus} />}</button>
+        </div>
       </nav>
       {modalOpen && (
         <Modal closeModal={closeModal}>
